fa400d91e3ba7897b3d841dca254846715b150d1
Otávio Cipriano <otavio.b.cipriano@gmail.com>
2022-07-22 17:44:48 -0300

    Add Validation on Lobby Form

---
+++ client/src/components/FormError/FormError.jsx
@@ -0,0 +1,3 @@
+export default function FormError({formError}) {
+  return <span>aa</span>
+}
--- client/src/components/LobbyBox/LobbyBox.jsx
+++ client/src/components/LobbyBox/LobbyBox.jsx
@@ -4,23 +4,47 @@ import Button from "react-bootstrap/Button";
 
 import { useUser } from '../../context/UserProvider'
 import { useState } from "react";
+import FormError from "../FormError/FormError";
 
 
 export default function LobbyBox() {
     const { setUser, user, room, setRoom } = useUser()
     const [username, setUsername] = useState('')
-    const [roomToJoin, setRoomToJoin] = useState('')
+    const [roomName, setRoomName] = useState('')
+    const [formError, setFormError] = useState({ username: '', room: '' })
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        if (validateForm()) return;
         setUser(username)
-        setRoom(roomToJoin)
-        setUsername('')
-        setRoomToJoin('')
+        setRoom(roomName)
+    }
+
+    const validateForm = () => {
+        if (!username && !roomName) {
+            setFormError({
+                username: "It\'s need to enter a username",
+                room: "It\'s need to enter a room name"
+            })
+
+            return true;
+        }
+        else if (!username || username.length < 4) {
+            setFormError({
+                username: "It\'s need to enter a username longer than 4 characters",
+                room: ""
+            })
+        }
+        else if (!roomName || roomName.length < 4) {
+            setFormError({
+                username: "",
+                room: "It\'s need to enter a room name longer than 4 characters"
+            })
+        }
     }
 
     return (
-        <Stack 
+        <Stack
             className="lobby__box rounded-3 text-center justify-content-md-center w-50 mx-auto pe-4 ps-4 pb-4 bg-gray-900 text-white" style={{ alignSelf: 'auto' }}>
             <h1 className='h2 mb-3 mt-5'>Lobby</h1>
             <Form onSubmit={handleSubmit}>
@@ -31,16 +55,23 @@ export default function LobbyBox() {
                         onChange={(e) => setUsername(e.currentTarget.value)}
                         type='text'
                         placeholder="Enter your Username"
-                        className="placeholder-white bg-secondary border-secondary text-white" />
+                        className={`placeholder-white bg-secondary border-secondary text-white ${formError['username'].length > 0 ? 'border-danger' : ''}`} />
+                    {formError['username'].length > 0 ?
+                        <Form.Text className="text-danger">{formError['username']}</Form.Text> :
+                        ''}
                 </Form.Group>
                 <Form.Group className='mb-3 text-start'>
                     <Form.Label>Room</Form.Label>
                     <Form.Control
-                        value={roomToJoin}
-                        onChange={(e) => setRoomToJoin(e.currentTarget.value)}
+                        value={roomName}
+                        onChange={(e) => setRoomName(e.currentTarget.value)}
                         type='text'
                         placeholder="Room to Join"
-                        className="placeholder-white bg-secondary border-secondary text-white" />
+                        className={`placeholder-white bg-secondary border-secondary text-white ${formError['username'].length > 0 ? 'border-danger' : ''}`} />
+                    {formError['room'].length > 0 ? 
+                    <Form.Text className="text-danger">{formError['room']}</Form.Text> : 
+                    ''}
+
                 </Form.Group>
                 <Button className="mt-4 pt-2 pb-2 ps-4 pe-4" type='submit' variant='primary'>Enter Chat</Button>
             </Form>
